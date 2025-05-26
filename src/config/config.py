ad0cd2ff5907c6d1e7f83920cf60ca82058be812
Noel <noel.gomaku@gmail.com>
2025-05-26 09:49:18 +0200

    ffmpeg ill frames stop condition

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from dataclasses import dataclass
 from config.rtsp import RTSPSettings
@@ -9,6 +10,8 @@ load_dotenv()
 @dataclass
 class Settings:
     REDIS: RedisConfig = RedisConfig()
+    MAX_ILL_FRAMES: int = os.getenv("MAX_ILL_FRAMES", 100)
+    CAMERA_SHUTDOWN_MAX_TIME: int = os.getenv("CAMERA_SHUTDOWN_MAX_TIME", 20)
 
 
 settings = Settings()
